4e07d2ff0807df7f083286a359feebbb72e13404
Jubayed Hossain <jubayed@users.noreply.github.com>
2017-07-30 01:16:15 +0600

    Create viewforum.php

---
+++ viewforum.php
@@ -0,0 +1,50 @@
+<?php
+
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'ACTIVE_TOPICS'			=> 'Active topics',
+	'ANNOUNCEMENTS'			=> 'Announcements',
+
+	'FORUM_PERMISSIONS'		=> 'Forum permissions',
+
+	'ICON_ANNOUNCEMENT'		=> 'Announcement',
+	'ICON_STICKY'			=> 'Sticky',
+
+	'LOGIN_NOTIFY_FORUM'	=> 'You have been notified about this forum, please login to view it.',
+
+	'MARK_TOPICS_READ'		=> 'Mark topics read',
+
+	'NEW_POSTS_HOT'			=> 'New posts [ Popular ]',	// Not used anymore
+	'NEW_POSTS_LOCKED'		=> 'New posts [ Locked ]',	// Not used anymore
+	'NO_NEW_POSTS_HOT'		=> 'No new posts [ Popular ]',	// Not used anymore
+	'NO_NEW_POSTS_LOCKED'	=> 'No new posts [ Locked ]',	// Not used anymore
+	'NO_READ_ACCESS'		=> 'You do not have the required permissions to read topics within this forum.',
+	'NO_UNREAD_POSTS_HOT'		=> 'No unread posts [ Popular ]',
+	'NO_UNREAD_POSTS_LOCKED'	=> 'No unread posts [ Locked ]',
+
+	'POST_FORUM_LOCKED'		=> 'Forum is locked',
+
+	'TOPICS_MARKED'			=> 'The topics for this forum have now been marked read.',
+
+	'UNREAD_POSTS_HOT'		=> 'Unread posts [ Popular ]',
+	'UNREAD_POSTS_LOCKED'	=> 'Unread posts [ Locked ]',
+
+	'VIEW_FORUM'			=> 'View forum',
+	'VIEW_FORUM_TOPICS'		=> array(
+		1	=> '%d topic',
+		2	=> '%d topics',
+	),
+));
