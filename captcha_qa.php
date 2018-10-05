5ceb8b9c2ad392bf3199ccab5e4ead111a5ee1d3
Jahedul Islam <jahed111@gmail.com>
2018-10-05 19:48:46 +0600

    Update captcha_qa.php

--- captcha_qa.php
+++ captcha_qa.php
@@ -10,7 +10,6 @@
 * the docs/CREDITS.txt file.
 *
 */
-
 /**
 * DO NOT CHANGE
 */
@@ -18,12 +17,10 @@ if (!defined('IN_PHPBB'))
 {
 	exit;
 }
-
 if (empty($lang) || !is_array($lang))
 {
 	$lang = array();
 }
-
 // DEVELOPERS PLEASE NOTE
 //
 // All language files should use UTF-8 as their encoding and the files must not contain a BOM.
@@ -35,18 +32,15 @@ if (empty($lang) || !is_array($lang))
 // You do not need this where single placeholders are used, e.g. 'Message %d' is fine
 // equally where a string contains only two placeholders which are used to wrap text
 // in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
-
 $lang = array_merge($lang, array(
 	'CAPTCHA_QA'				=> 'Q&amp;A',
-	'CONFIRM_QUESTION_EXPLAIN'	=> 'This question is a means of preventing automated form submissions by spambots.',
-	'CONFIRM_QUESTION_WRONG'	=> 'You have provided an invalid answer to the question.',
-	'CONFIRM_QUESTION_MISSING'	=> 'Questions for the captcha could not be retrieved. Please contact a board administrator.',
-
-	'QUESTION_ANSWERS'			=> 'Answers',
-	'ANSWERS_EXPLAIN'			=> 'Please enter valid answers to the question, one per line.',
-	'CONFIRM_QUESTION'			=> 'Question',
-
-	'ANSWER'					=> 'Answer',
+	'CONFIRM_QUESTION_EXPLAIN'	=> 'এই প্রশ্ন স্পাম বটস দ্বারা স্বয়ংক্রিয় ফর্ম জমা প্রতিরোধ করার একটি উপায়।',
+	'CONFIRM_QUESTION_WRONG'	=> 'আপনি প্রশ্নের একটি ভুল উত্তর প্রদান করেছেন।',
+	'CONFIRM_QUESTION_MISSING'	=> 'ক্যাপচার জন্য প্রশ্ন পুনরুদ্ধার করা যায়নি। দয়া করে একজন বোর্ড প্রশাসকের সাথে যোগাযোগ করুন।',
+	'QUESTION_ANSWERS'			=> 'উত্তরগুলো',
+	'ANSWERS_EXPLAIN'			=> 'দয়া করে প্রতি লাইনে একটি বৈধ উত্তর লিখুন।',
+	'CONFIRM_QUESTION'			=> 'প্রশ্ন',
+	'ANSWER'					=> 'উত্তর',
 	'EDIT_QUESTION'				=> 'Edit Question',
 	'QUESTIONS'					=> 'Questions',
 	'QUESTIONS_EXPLAIN'			=> 'For every form submission where you have enabled the Q&amp;A plugin, users will be asked one of the questions specified here. To use this plugin at least one question must be set in the default language. These questions should be easy for your target audience to answer but beyond the ability of a bot capable of running a Google™ search. Using a large and regularly changed set of questions will yield the best results. Enable the strict setting if your question relies on mixed case, punctuation or whitespace.',
@@ -55,10 +49,8 @@ $lang = array_merge($lang, array(
 	'QUESTION_LANG_EXPLAIN'		=> 'The language this question and its answers are written in.',
 	'QUESTION_STRICT'			=> 'Strict check',
 	'QUESTION_STRICT_EXPLAIN'	=> 'Enable to enforce mixed case, punctuation and whitespace.',
-
 	'QUESTION_TEXT'				=> 'Question',
 	'QUESTION_TEXT_EXPLAIN'		=> 'The question presented to the user.',
-
 	'QA_ERROR_MSG'				=> 'Please fill in all fields and enter at least one answer.',
 	'QA_LAST_QUESTION'			=> 'You cannot delete all questions while the plugin is active.',
 ));
